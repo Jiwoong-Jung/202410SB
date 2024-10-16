3424c062d5d2d1a5a0ac2077792878d829732827
jiwoong-jung <jjw981119@naver.com>
2024-10-16 14:37:36 +0900

    1016

--- sb1015_2/src/main/java/edu/du/sb1015_2/controller/HeloController.java
+++ sb1015_2/src/main/java/edu/du/sb1015_2/controller/HeloController.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -26,6 +23,12 @@ public class HeloController {
         List<MyData> list = repository.findAll();
         model.addAttribute("datalist",list);
         return "index";
+    }
+
+    @PostMapping("/")
+    public String form(@ModelAttribute("formModel") MyData mydata) {
+        repository.saveAndFlush(mydata);
+        return "redirect:/";
     }
 
 }
