b0ede133f68c35612760ea7a477a768138d160db
jiwoong-jung <jjw981119@naver.com>
2024-10-22 16:34:13 +0900

    1022

--- sb1022secu4/src/main/java/edu/du/sb1022secu3/service/UserDetailsServiceImpl.java
+++ sb1022secu4/src/main/java/edu/du/sb1022secu3/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package edu.du.sb1022secu3.service;
 
+import edu.du.sb1022secu3.entity.Member;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,16 +13,34 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 public class UserDetailsServiceImpl implements UserDetailsService {
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("==========>사용자: " + username);
-        UserDetails user = User.withUsername("user123")
+//        UserDetails user = User.withUsername("user123")
+//                .password(passwordEncoder().encode("1234"))
+//                .roles("ADMIN")
+//                .build();
+//        return user;
+        Member member = Member.builder()
+                .id(1001L)
+                .username("홍길동")
                 .password(passwordEncoder().encode("1234"))
-                .roles("ADMIN")
+                .email("hong@aaa.com")
                 .build();
-        return user;
+        return toUserDetails(member);
+
     }
     private PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private UserDetails toUserDetails(Member member) {
+        return User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+//                .authorities(new SimpleGrantedAuthority(member.getRole().toString()))
+                .roles("USER")
+                .build();
     }
 }
