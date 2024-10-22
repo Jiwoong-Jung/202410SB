efa122e7d88847c1658a966e9cc57e45ddf0bf6b
jiwoong-jung <jjw981119@naver.com>
2024-10-22 16:41:44 +0900

    1022

--- sb1022secu4/src/main/java/edu/du/sb1022secu3/controller/BeginController.java
+++ sb1022secu4/src/main/java/edu/du/sb1022secu3/controller/BeginController.java
@@ -1,13 +1,38 @@
 package edu.du.sb1022secu3.controller;
 
+import edu.du.sb1022secu3.entity.Member;
+import edu.du.sb1022secu3.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 public class BeginController {
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @GetMapping("/")
     public String index() {
         return "/sample/all";
+    }
+
+    @PostConstruct
+    public void init() {
+        Member member = Member.builder()
+                .id(1001L)
+                .username("hong1")
+                .password(passwordEncoder().encode("1234"))
+                .email("hong1@aaa.com")
+                .build();
+        memberRepository.save(member);
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
