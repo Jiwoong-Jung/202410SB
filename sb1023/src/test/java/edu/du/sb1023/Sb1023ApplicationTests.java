772209e7addacbffe56710bea40ff4eec37266fd
jiwoong-jung <jjw981119@naver.com>
2024-10-23 15:27:13 +0900

    1023

--- sb1023/src/test/java/edu/du/sb1023/Sb1023ApplicationTests.java
+++ sb1023/src/test/java/edu/du/sb1023/Sb1023ApplicationTests.java
@@ -24,7 +24,19 @@ class Sb1023ApplicationTests {
         transaction.begin();
 
         // em.persist() 사용하여 데이터를 입력해 보세요.
+        // 팀1 저장
+        Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
 
+        // 회원1 저장
+        Member member1 = new Member("member1", "회원1");
+        member1.setTeam(team1);  // 연관관계 설정 member1 -> team1
+        em.persist(member1);
+
+        // 회원2 저장
+        Member member2 = new Member("member2", "회원2");
+        member2.setTeam(team1);  // 연관관계 설정 member2 -> team1
+        em.persist(member2);
         transaction.commit();
     }
 
